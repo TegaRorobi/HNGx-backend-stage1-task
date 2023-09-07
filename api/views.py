1d34c402dc45c1cf5c885593e963d023e2d73033
Tega Rorobi <rorobitega@gmail.com>
2023-09-07 23:15:36 +0100

    Ran migrations and created the endpoint at /api/

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import datetime
 
 # Create your views here.
+def api_view(request):
+	data = {
+		'slack_name': request.GET.get('slack_name'),
+		'current_day': datetime.datetime.today().strftime('%A'),
+		'utc_time': datetime.datetime.utcnow().isoformat().split('.')[0]+'Z',
+		'track': request.GET.get('track'),
+		'github_file_url': 'https://github.com/TegaRorobi/HNGx-backend-stage1-task/blob/master/api/views.py',
+		'github_repo_url': 'https://github.com/TegaRorobi/HNGx-backend-stage1-task/',
+		'status_code': 200
+	}
+
+	return JsonResponse(data)
--- src/urls.py
+++ src/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import api_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api_view)
 ]
